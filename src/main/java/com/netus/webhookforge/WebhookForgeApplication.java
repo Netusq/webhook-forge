1ab8c45e0b4c981175e5f680253c9f67ad7c9680
Netusq <tor.netus@gmail.com>
2026-09-12 17:28:22 +0700

    Initial commit

---
+++ src/main/java/com/netus/webhookforge/WebhookForgeApplication.java
@@ -0,0 +1,13 @@
+package com.netus.webhookforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netus/webhookforge/WebhookForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netus.webhookforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
